d6b4818ab87c766417b6dee03e70babd2c58821b
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2018-01-16 17:11:59 -0800

    first round of working site

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('.carousel').carousel();
+	$('.parallax').parallax();
+	$(".button-collapse").sideNav();
+	$('.modal').modal();
+
+});
+
+
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
+ 
